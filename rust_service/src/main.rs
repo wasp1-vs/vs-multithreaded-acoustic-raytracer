f6a09b0fa0b6b57a8830471d85f3370a0b96a78d
Noahqv2 <noah_osato.asemota@smail.th-koeln.de>
2026-06-04 20:08:53 +0200

    Merged feature branches into python dev, fixed some engine logic

--- rust_service/src/main.rs
+++ rust_service/src/main.rs
@@ -20,28 +20,43 @@ struct Cli {
     config_file: String,
 }
 fn main() {
-    
-    let test_wall1 = Wall {
-        start:Vec2::new(10.0,0.0),
-        end:Vec2::new(10.0,10.0),
-        absorption: 0.1
+    //
+    // let test_wall1 = Wall {
+    //     start:Vec2::new(50.0,0.0),
+    //     end:Vec2::new(50.0,50.0),
+    //     absorption: 1.0
+    // };
+    // let test_wall2 = Wall {
+    //     start:Vec2::new(50.0,50.0),
+    //     end:Vec2::new(0.0,50.0),
+    //     absorption: 1.0
+    // };
+    // let test_wall3 = Wall {
+    //     start:Vec2::new(0.0,50.0),
+    //     end:Vec2::new(0.0,0.0),
+    //     absorption: 1.0
+    // };
+    // let test_wall4 = Wall {
+    //     start:Vec2::new(0.0,0.0),
+    //     end:Vec2::new(50.0,0.0),
+    //     absorption: 1.0
+    // };
+    // let room: Vec<Wall> = vec![test_wall1, test_wall2, test_wall3, test_wall4];
+    // The reflective canyon wall we want to test
+    let front_wall = Wall {
+        start: Vec2::new(343.0, -1000.0),
+        end: Vec2::new(343.0, 1000.0),
+        absorption: 0.0
     };
-    let test_wall2 = Wall {
-        start:Vec2::new(10.0,10.0),
-        end:Vec2::new(0.0,10.0),
-        absorption: 0.1
+
+    // The "catcher" wall behind the speaker.
+    // Set absorption to 1.0 so the ray dies immediately after hitting it.
+    let back_wall = Wall {
+        start: Vec2::new(-10.0, -1000.0),
+        end: Vec2::new(-10.0, 1000.0),
+        absorption: 0.0
     };
-    let test_wall3 = Wall {
-        start:Vec2::new(0.0,10.0),
-        end:Vec2::new(0.0,0.0),
-        absorption: 0.1
-    };
-    let test_wall4 = Wall {
-        start:Vec2::new(0.0,0.0),
-        end:Vec2::new(10.0,0.0),
-        absorption: 0.1
-    };
-    let room: Vec<Wall> = vec![test_wall1, test_wall2, test_wall3, test_wall4];
+    let room: Vec<Wall> = vec![front_wall, back_wall];
     let cli = Cli::parse();
     println!("Reading config from: {}", cli.config_file);
     let config_text = fs::read_to_string(&cli.config_file)
@@ -50,11 +65,11 @@ fn main() {
         .expect("Could not parse simulation config (Is the JSON formatted correctly?)");
 
 
-    println!("Starting single simulation");
-    let start_single = Instant::now();
-    let(_delays_single, _pressures_single) = simulation::run_simulation_single(&test_config, &room);
-    let duration_singe = start_single.elapsed().as_secs_f32();
-    println!("Simulation run time: {} s", duration_singe);
+    // println!("Starting single simulation");
+    // let start_single = Instant::now();
+    // let(_delays_single, _pressures_single) = simulation::run_simulation_single(&test_config, &room);
+    // let duration_singe = start_single.elapsed().as_secs_f32();
+    // println!("Simulation run time: {} s", duration_singe);
 
     println!("Starting parallel simulation");
     let start_parallel = Instant::now();
@@ -62,8 +77,8 @@ fn main() {
     let duration_parallel = start_parallel.elapsed().as_secs_f32();
     println!("Simulation run time: {} s", duration_parallel);
 
-    let speedup = duration_singe / duration_parallel;
-    println!("\nSpeedup time: {:.2}x s", speedup);
+    // let speedup = duration_singe / duration_parallel;
+    // println!("\nSpeedup time: {:.2}x s", speedup);
 
     export::export_results(delays_par, pressures_par, &test_config);
 
